a41a70bc9f53e7f5a2e40b86eaede0f25dde5db8
Christopher George Steel <chrissteel@improbable.io>
2022-06-12 23:16:18 +0100

    Started adding express

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import mikroConfig from "./mikro-orm.config"
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     orm.getMigrator().up();
     const entityManager = orm.em.fork({});
-    const post = entityManager.create(Post, {title: "test"});
-    await entityManager.persistAndFlush(post);
+    entityManager.flush();
 
-    const posts = await entityManager.find(Post, {});
-    console.log(posts);
+    const app = express();
+    app.get("/", (_, res) => {
+        res.send("yo");
+    });
+    app.listen(4200, () => {
+        console.log("Server started on localhost:4200");
+    });
 }
 
 main();
